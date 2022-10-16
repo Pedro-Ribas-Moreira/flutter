ae5e5ad2836d731d126aaea03adeecbac60dc6b8
Pedro Moreira <phribasmoreira@gmail.com>
2022-10-16 17:19:47 +0100

    added images and decoration tag, improving styling in the app.

--- project_01/lib/main.dart
+++ project_01/lib/main.dart
@@ -17,19 +17,31 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
+          leading: Container(),
           title: Text("Tarefas"),
         ),
-        body: ListView(
-          children: [
-            Task("Aprender Flutter"),
-            Task("Criar meu primeiro app"),
-            Task("Subir para o github"),
-            Task("Adicionar ao portfolio"),
-            Task("Adicionar ao portfolio"),
-            Task("Adicionar ao portfolio"),
-            Task("Adicionar ao portfolio lorem ipsum de ary in mare"),
-            Task("Adicionar ao portfolio"),
-          ],
+        body: Container(
+          color: const Color(0xffE5E5E5),
+          child: ListView(
+            children: [
+              Task(
+                  "Aprender Flutter",
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                  5),
+              Task(
+                  "Criar meu primeiro app",
+                  "https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg",
+                  2),
+              Task(
+                  "Subir para o github",
+                  "https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg",
+                  3),
+              Task(
+                  "Adicionar ao portfolio",
+                  "https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg",
+                  5),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
@@ -41,7 +53,10 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatefulWidget {
   final String name;
-  const Task(this.name, {Key? key}) : super(key: key);
+  final String img;
+  final int importance;
+  const Task(this.name, this.img, this.importance, {Key? key})
+      : super(key: key);
 
   @override
   State<Task> createState() => _TaskState();
@@ -57,36 +72,104 @@ class _TaskState extends State<Task> {
       child: Container(
         child: Stack(
           children: [
-            Container(color: Colors.blue, height: 140),
+            Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4), color: Colors.blue),
+                height: 140),
             Column(
               children: [
                 Container(
-                  color: Colors.white,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      color: Colors.white),
                   height: 100,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        color: Colors.black26,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: Colors.black26),
                         width: 72,
                         height: 100,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(
+                            widget.img,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                      Container(
-                          width: 200,
-                          child: Text(
-                            widget.name,
-                            style: const TextStyle(
-                                fontSize: 24, overflow: TextOverflow.fade),
-                          )),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              lvl++;
-                            });
-
-                            print(lvl);
-                          },
-                          child: const Icon(Icons.arrow_drop_up))
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 200,
+                            child: Text(
+                              widget.name,
+                              style: const TextStyle(
+                                  fontSize: 24, overflow: TextOverflow.fade),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.importance >= 1)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.importance >= 2)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.importance >= 3)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.importance >= 4)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.importance >= 5)
+                                      ? Colors.blue
+                                      : Colors.blue[100])
+                            ],
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: SizedBox(
+                          height: 52,
+                          width: 52,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    lvl++;
+                                  },
+                                );
+                              },
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: const [
+                                  Icon(Icons.arrow_drop_up),
+                                  Text(
+                                    "Up",
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ],
+                              )),
+                        ),
+                      )
                     ],
                   ),
                 ),
@@ -99,12 +182,14 @@ class _TaskState extends State<Task> {
                         width: 200,
                         child: LinearProgressIndicator(
                           color: Colors.white,
-                          value: lvl / 10,
+                          value: (widget.importance > 0)
+                              ? (lvl / widget.importance) / 10
+                              : 1,
                         ),
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.all(12),
+                      padding: const EdgeInsets.all(12),
                       child: Text(
                         "Level: $lvl",
                         style:
